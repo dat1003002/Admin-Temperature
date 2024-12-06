1927c7bf4b32a79ab3672a5eabcf65319476fc64
dat1003002 <phanvandat10032002@gmail.com>
2024-12-06 13:25:23 +0700

    Track new files and update .gitignore

---
+++ TemperatureApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TemperatureApp.Models;
+namespace TemperatureApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+        public DbSet<MS_1> MS_1 { get; set; }
+        public DbSet<MS_2> MS_2 { get; set; }
+        public DbSet<MS_3> MS_3 { get; set; }
+    }
+}
